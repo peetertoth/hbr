2078c633a0bf042a8fbee4269464046d6a0a1199
PeterToth <peeter.toth@gmail.com>
2019-10-03 01:33:38 +0200

    Initialize mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
 // **Import
 const express = require('express');
 const bodyParser = require('body-parser');
-// **Import End
+const mongoose = require('mongoose');
+// Import**
 
 // Server instance
 const app = express();
 
 // Middleware for express server
 app.use(bodyParser.json());
+
+// Init mongodb connection
+const mongodbUrl = process.env.MONGODB_URL || 'mongodb://localhost/hbr';
+console.info(`Initialize mongodb connection to ${mongodbUrl} ...`);
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+// **Endpoints
 
 app.get('/', (req, res) => {
     console.log('Homepage open');
